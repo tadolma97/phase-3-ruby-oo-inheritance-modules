8d39d39b93858465d754932907c6b3269c139446
Tashi Dolma <tadolma97@gmail.com>
2022-06-09 15:55:43 -0400

    commit tree bed85a3550a1fd91297eaf964283a94c1a82934c

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,18 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+# class Dancer
+#     include Dance
+#     extend MetaDancing
+    
+#     attr_accessor :name
+
+#     def initialize(name)
+#         @name = name
+#     end
+# end
+
+class Dancer
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+  end
